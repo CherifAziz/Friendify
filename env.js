a14c8d2a4a7525448b5caff3c65e2af92c8efa6b
CherifAziz <mohamed-aziz.cherif@epitech.eu>
2023-03-09 16:36:40 +0100

    [MOD] - change driectory

--- env.js
+++
@@ -1,9 +0,0 @@
-export const fbConfig = {
-  apiKey: "AIzaSyDCYazdQPs9Pltjx65fo-hZtwmuij6V7zM",
-  authDomain: "friendify-e9ac8.firebaseapp.com",
-  databaseURL: "https://friendify-e9ac8-default-rtdb.firebaseio.com",
-  projectId: "friendify-e9ac8",
-  storageBucket: "friendify-e9ac8.appspot.com",
-  messagingSenderId: "585147108403",
-  appId: "1:585147108403:web:c5c119bac83a8b7bc8a746",
-};
